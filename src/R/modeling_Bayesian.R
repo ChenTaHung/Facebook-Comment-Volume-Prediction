40aeca5b11af5a1c01a62c26dcf9c698dbb4c3d2
ChenTaHung <denny20700@gmail.com>
2023-12-14 02:55:19 -0500

    add stritified k-fold

--- src/R/modeling_Bayesian.R
+++ src/R/modeling_Bayesian.R
@@ -50,22 +50,31 @@ save(NegBinBayes_laplace, file = 'data/Model-Object/Bayes/NegBinBayes_laplace.RD
 
 # small samples -----------------------------------------------------------
 set.seed(2023)
-small_fv5 <- fv5_train[sample(c(1:nrow(fv5_train)), 10000),]
+folds <- caret::createFolds(y = fv5_train$Page_Category, k =  10)
+small_fv5 <- fv5_train[folds["Fold01"]$Fold01, ]
 
+PoissonBayesFit_small <- rstanarm::stan_glmer(Target_Variable ~ CC1_logNorm + CC2_logNorm + CC3_logNorm + CC4_logNorm + CC5 + 
+                                               Page_Popularity_Likes_logNorm + Page_Checkins_logNorm +  Page_Talking_About_logNorm + 
+                                               Post_Length_logNorm + Post_Share_Count_logNorm + 
+                                               CC2_per_hr_logNorm + CC3_per_hr_logNorm + CC4_per_hr_logNorm + 
+                                                offset(log(Base_Time+1)) + (1|Category),
+                                               data = small_fv5, family = poisson(link = "log"))
+save(PoissonBayesFit_small, file = 'data/Model-Object/Bayes/PoisBayes_small.RDS')
 
 NegBinBayesFit_small <- rstanarm::stan_glmer(Target_Variable ~ CC1_logNorm + CC2_logNorm + CC3_logNorm + CC4_logNorm + CC5 + 
-                                                  Page_Popularity_Likes_logNorm + Page_Checkins_logNorm +  Page_Talking_About_logNorm + 
-                                                  Post_Length_logNorm + Post_Share_Count_logNorm + 
-                                                  CC2_per_hr_logNorm + CC3_per_hr_logNorm + CC4_per_hr_logNorm + (1|Category),
+                                               Page_Popularity_Likes_logNorm + Page_Checkins_logNorm +  Page_Talking_About_logNorm + 
+                                               Post_Length_logNorm + Post_Share_Count_logNorm + 
+                                               CC2_per_hr_logNorm + CC3_per_hr_logNorm + CC4_per_hr_logNorm + 
+                                                offset(log(Base_Time+1)) + (1|Category),
                                                  data = small_fv5, family = neg_binomial_2(link = "log"))
 
-
-save(NegBinBayesFit_small, file = 'data/Model-Object/Bayes/NegBinBayes_laplace_small.RDS')
+save(NegBinBayesFit_small, file = 'data/Model-Object/Bayes/NegBinBayes_small.RDS')
 
 NegBinBayes_laplace_small <- rstanarm::stan_glmer(Target_Variable ~ CC1_logNorm + CC2_logNorm + CC3_logNorm + CC4_logNorm + CC5 + 
-                                                     Page_Popularity_Likes_logNorm + Page_Checkins_logNorm +  Page_Talking_About_logNorm + 
-                                                     Post_Length_logNorm + Post_Share_Count_logNorm + 
-                                                     CC2_per_hr_logNorm + CC3_per_hr_logNorm + CC4_per_hr_logNorm + (1|Category),
+                                                       Page_Popularity_Likes_logNorm + Page_Checkins_logNorm +  Page_Talking_About_logNorm + 
+                                                       Post_Length_logNorm + Post_Share_Count_logNorm + 
+                                                       CC2_per_hr_logNorm + CC3_per_hr_logNorm + CC4_per_hr_logNorm + 
+                                                        offset(log(Base_Time+1)) + (1|Category),
                                                    data = small_fv5, family = neg_binomial_2(link = "log"),
                                                    prior = laplace(scale = 1, autoscale = TRUE))
 
