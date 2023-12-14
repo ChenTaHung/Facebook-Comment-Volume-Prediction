4afbe2fd21551d9d0f3a461b96546de55d9498eb
ChenTaHung <denny20700@gmail.com>
2023-12-14 01:56:45 -0500

    adjust dataset size

--- src/R/modeling_Bayesian.R
+++ src/R/modeling_Bayesian.R
@@ -3,6 +3,7 @@ source('src/R/Bayes-Model-Functions.R')
 
 library(rstanarm)
 library(rstan)
+library(tidyverse)
 
 fv5_train1 <- read_csv('~/Desktop/MSSP/MA678-AppliedStatisticalModeling/Final-Project/Remote-Git/data/Dataset/fv5_train1.csv')
 fv5_train2 <- read_csv('~/Desktop/MSSP/MA678-AppliedStatisticalModeling/Final-Project/Remote-Git/data/Dataset/fv5_train2.csv')
@@ -11,7 +12,6 @@ fv5_train <- rbind(fv5_train1, fv5_train2)
 
 
 # Modeling ----------------------------------------------------------------
-tmp <- fv5_train[sample(c(1:nrow(fv5_train)), 5000),]
 
 options(mc.cores = 4)
 
@@ -38,11 +38,36 @@ pp_check(NegBinBayesFit)
 # NB laplace prior --------------------------------------------------------
 
 
-NegBinBayes_laplace_mse <- customizedRandomizedSearchCV(data = fv5_train, 
-                                                       formula = Target_Variable ~ CC1_logNorm + CC2_logNorm + CC3_logNorm + CC4_logNorm + CC5 + 
-                                                            Page_Popularity_Likes_logNorm + Page_Checkins_logNorm +  Page_Talking_About_logNorm + 
-                                                            Post_Length_logNorm + Post_Share_Count_logNorm + 
-                                                            CC2_per_hr_logNorm + CC3_per_hr_logNorm + CC4_per_hr_logNorm + (1|Category),
-                                                        fold_number = 5, stratified_target = 'Category',
-                                                        MCMC_parms = list(chains = 4, iter = 2000, refresh = 1, adapt_delta = 0.95, QR = TRUE, sparse = FALSE),
-                                                        randomseed = 2023, lambda_dist = rexp, n = 5, rate = 0.8)
+NegBinBayes_laplace <- rstanarm::stan_glmer(Target_Variable ~ CC1_logNorm + CC2_logNorm + CC3_logNorm + CC4_logNorm + CC5 + 
+                                               Page_Popularity_Likes_logNorm + Page_Checkins_logNorm +  Page_Talking_About_logNorm + 
+                                               Post_Length_logNorm + Post_Share_Count_logNorm + 
+                                               CC2_per_hr_logNorm + CC3_per_hr_logNorm + CC4_per_hr_logNorm + (1|Category),
+                                             data = fv5_train, family = neg_binomial_2(link = "log"),
+                                             prior = laplace(scale = 1, autoscale = TRUE))
+
+save(NegBinBayes_laplace, file = 'data/Model-Object/Bayes/NegBinBayes_laplace.RDS')
+
+
+# small samples -----------------------------------------------------------
+set.seed(2023)
+small_fv5 <- fv5_train[sample(c(1:nrow(fv5_train)), 10000),]
+
+
+NegBinBayesFit_small <- rstanarm::stan_glmer(Target_Variable ~ CC1_logNorm + CC2_logNorm + CC3_logNorm + CC4_logNorm + CC5 + 
+                                                  Page_Popularity_Likes_logNorm + Page_Checkins_logNorm +  Page_Talking_About_logNorm + 
+                                                  Post_Length_logNorm + Post_Share_Count_logNorm + 
+                                                  CC2_per_hr_logNorm + CC3_per_hr_logNorm + CC4_per_hr_logNorm + (1|Category),
+                                                 data = small_fv5, family = neg_binomial_2(link = "log"))
+
+
+save(NegBinBayesFit_small, file = 'data/Model-Object/Bayes/NegBinBayes_laplace_small.RDS')
+
+NegBinBayes_laplace_small <- rstanarm::stan_glmer(Target_Variable ~ CC1_logNorm + CC2_logNorm + CC3_logNorm + CC4_logNorm + CC5 + 
+                                                     Page_Popularity_Likes_logNorm + Page_Checkins_logNorm +  Page_Talking_About_logNorm + 
+                                                     Post_Length_logNorm + Post_Share_Count_logNorm + 
+                                                     CC2_per_hr_logNorm + CC3_per_hr_logNorm + CC4_per_hr_logNorm + (1|Category),
+                                                   data = small_fv5, family = neg_binomial_2(link = "log"),
+                                                   prior = laplace(scale = 1, autoscale = TRUE))
+
+save(NegBinBayes_laplace_small, file = 'data/Model-Object/Bayes/NegBinBayes_laplace_small.RDS')
+
